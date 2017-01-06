a51d8eb752eb0d14ed4d1ea3e06a48da6bb0dc3e
mrMekdad <mekdadr6@gmail.com>
2017-01-06 16:21:00 +0100

    campus-java-manager: introduce core source layout

---
+++ src/main/java/reda/campusjavamanager/ProjectRecord.java
@@ -0,0 +1,11 @@
+package reda.campusjavamanager;
+
+public class ProjectRecord {
+    public final String sampleId;
+    public final String status;
+
+    public ProjectRecord(String sampleId, String status) {
+        this.sampleId = sampleId;
+        this.status = status;
+    }
+}
